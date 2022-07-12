16cee1d23eb206db9b46669ed5fb3229d21cb3f1
Lucas Bernardo <49166263+lucasber80@users.noreply.github.com>
2022-07-12 13:54:43 -0300

    .

--- src/app/pages/ncs/about/about.component.ts
+++ src/app/pages/ncs/about/about.component.ts
@@ -1,9 +1,9 @@
-import { RoleGuardService } from './../../../_services/role-guard.service';
-import { TokenStorageService } from 'src/app/_services/token-storage.service';
-import { Component, OnInit } from '@angular/core';
-import { NonComplianceService } from 'src/app/_services/non-compliance.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import {AboutService} from "./about.service";
+import { RoleGuardService } from "./../../../_services/role-guard.service";
+import { TokenStorageService } from "src/app/_services/token-storage.service";
+import { Component, OnInit } from "@angular/core";
+import { NonComplianceService } from "src/app/_services/non-compliance.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AboutService } from "./about.service";
 
 @Component({
   selector: "app-about",
@@ -16,8 +16,8 @@ export class AboutComponent implements OnInit {
     private route: ActivatedRoute,
     public aboutSrvc: AboutService,
     private router: Router,
-    private tokenService : TokenStorageService,
-    public roleService : RoleGuardService 
+    private tokenService: TokenStorageService,
+    public roleService: RoleGuardService
   ) {}
 
   ngOnInit(): void {
@@ -26,74 +26,113 @@ export class AboutComponent implements OnInit {
   }
 
   goCreateProp(e: any) {
-     let id = parseInt(this.route.snapshot.paramMap.get("id") || "");
+    let id = parseInt(this.route.snapshot.paramMap.get("id") || "");
     if (e["index"] == 1 && !this.aboutSrvc.hasProposal()) {
       this.router.navigate(["/", "ncs", "createProp", id]);
     }
-    console.log(this.aboutSrvc.nc?.proposalSolution)
-    if (e["index"] == 3 && this.aboutSrvc.nc?.proposalSolution && !this.aboutSrvc.nc?.sgqEvaluation) {
+    console.log(this.aboutSrvc.nc?.proposalSolution);
+    if (
+      e["index"] == 3 &&
+      this.aboutSrvc.nc?.proposalSolution &&
+      !this.aboutSrvc.nc?.sgqEvaluation
+    ) {
       this.router.navigate(["/", "ncs", "sgq", id]);
     }
   }
 
-  checkDisableProposal():boolean {
-    var disabled = true
-    
+  checkDisableProposal(): boolean {
+    var disabled = true;
+
     if (this.aboutSrvc.nc?.sector?.responsible_email) {
-      if(this.roleService.isResponsibleOrManager(this.aboutSrvc.nc.sector.responsible_email) || this.isActionPlanResponsible) {
-        disabled = false
+      if (
+        this.roleService.isResponsibleOrManager(
+          this.aboutSrvc.nc.sector.responsible_email
+        ) ||
+        this.isActionPlanResponsible()
+      ) {
+        disabled = false;
       }
     }
-    return disabled
+    return disabled;
   }
 
-  isActionPlanResponsible():boolean {
+  isActionPlanResponsible(): boolean {
+    var user = this.tokenService.getUser();
 
-    var user = this.tokenService.getUser()
+    var listActionPlan = this.aboutSrvc.nc.proposalSolution?.actionPlans;
 
-    var listActionPlan = this.aboutSrvc.nc.proposalSolution?.actionPlans
-
-    if(listActionPlan) {
-      for ( var i = 0; i < listActionPlan?.length; i++) {
+    if (listActionPlan) {
+      for (var i = 0; i < listActionPlan?.length; i++) {
         var actionPlanResponsibleEmail = listActionPlan[i].responsible;
 
         if (actionPlanResponsibleEmail) {
-          if(actionPlanResponsibleEmail == user['email']) return true
+          if (actionPlanResponsibleEmail == user["email"]) return true;
         }
-      }      
+      }
     }
 
-
-    return false
+    return false;
   }
 
-  checkDisableCosts():boolean {
+  checkDisableCosts(): boolean {
+    var disabled = true;
 
-
-    var disabled = true
-    
     if (this.aboutSrvc.hasProposal() && this.roleService.isFiscalOrManager()) {
-      disabled = false
+      let proposta = this.aboutSrvc.nc.proposalSolution;
+
+      if (
+        !!proposta?.root_cause &&
+        !!proposta?.effect_description &&
+        (!!proposta?.material_description ||
+          !!proposta?.man_power_description ||
+          !!proposta?.environment_description ||
+          !!proposta?.machine_description ||
+          !!proposta?.method_description ||
+          !!proposta?.measurement_description) &&
+        proposta?.actionPlans.length > 0
+      ) {
+        disabled = false;
+      }
     }
-    return disabled
+    return disabled;
   }
 
-  checkDisableSgqEval():boolean {
-    var disabled = true
-    
+  checkDisableSgqEval(): boolean {
+    var disabled = true;
+
     if (this.aboutSrvc.hasProposal() && this.roleService.isManager()) {
-      disabled = false
+      let proposta = this.aboutSrvc.nc.proposalSolution;
+
+      if (
+        !!proposta?.root_cause &&
+        !!proposta?.effect_description &&
+        (!!proposta?.material_description ||
+          !!proposta?.man_power_description ||
+          !!proposta?.environment_description ||
+          !!proposta?.machine_description ||
+          !!proposta?.method_description ||
+          !!proposta?.measurement_description) &&
+        proposta?.actionPlans.length > 0
+      ) {
+        disabled = false;
+      }
     }
-    return disabled
+    return disabled;
   }
 
+  checkDisableClose(): boolean {
+    var disabled = true;
 
-  checkDisableClose():boolean {
-    var disabled = true
-    
-    if (this.aboutSrvc.hasProposal() &&  this.aboutSrvc.hasSgqEval() && this.roleService.isManager()) {
-      disabled = false
+    if (
+      this.aboutSrvc.hasProposal() &&
+      this.aboutSrvc.hasSgqEval() &&
+      this.roleService.isManager()
+    ) {
+      let sgq = this.aboutSrvc.nc.sgqEvaluation;
+      if (!!sgq.text_area1 && !!sgq.text_area2 && !!sgq.text_area3) {
+        disabled = false;
+      }
     }
-    return disabled
+    return disabled;
   }
 }
