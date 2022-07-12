a6d5d7bd5f80b02bccd0547ead42906044c3ab0f
Parjasnelly A <63004038+parjasnelly@users.noreply.github.com>
2022-07-12 09:42:57 -0300

    bug fixes

--- src/app/pages/ncs/create-prop/steps/step1/ishikawa-diagram.component.ts
+++ src/app/pages/ncs/create-prop/steps/step1/ishikawa-diagram.component.ts
@@ -46,7 +46,7 @@ export class IshikawaDiagramComponent implements OnInit {
 
   returnString(obj: any) {
     let str = ''
-    if (obj?.name) str += obj.name + " ";
+    if (obj?.name) return obj.name + " ";
     if (obj?.code) str += obj.code + " ";
     if (obj?.description) str += obj.description + " ";
     if (obj?.rev) str += obj.rev + " ";
