3459dd150f2a58d80ace0ef31d4f9148015ed422
Parjasnelly A <63004038+parjasnelly@users.noreply.github.com>
2022-07-13 16:27:32 -0300

    bug fixes

--- src/app/pages/ncs/sgq-evaluation/sgq-header/sgq-header.component.ts
+++ src/app/pages/ncs/sgq-evaluation/sgq-header/sgq-header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {SgqService} from "../sgq.service";
 import {ActivatedRoute} from "@angular/router";
+import {MessageService} from "primeng/api";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-sgq-header',
@@ -11,15 +13,30 @@ export class SgqHeaderComponent implements OnInit {
     id: number;
 
   constructor(public sgqServ: SgqService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private messageService: MessageService,
+              public translate: TranslateService) { }
 
   ngOnInit(): void {
     this.id = parseInt(this.route.snapshot.paramMap.get('id')||"")
   }
 
   salvarSGQ() {
-    this.sgqServ.post().subscribe(next=>{
-      console.log(next)
+    this.sgqServ.post().subscribe({
+      next: data => {
+        this.messageService.add({
+          severity: "success",
+          summary: this.translate.instant("sgq.success"),
+          life: 3000,
+        });
+      },
+      error: err => {
+        this.messageService.add({
+          severity: "error",
+          summary: err,
+          life: 3000,
+        });
+      }
     })
   }
 }
